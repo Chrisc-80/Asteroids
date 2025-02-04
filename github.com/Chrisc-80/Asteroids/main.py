de4f5a212aa69355174e5ccb0fff73bc83762637
Chrisc-80 <chrisc@wp.pl>
2025-02-04 11:51:55 +0100

    add shot to asteroids

--- github.com/Chrisc-80/Asteroids/main.py
+++ github.com/Chrisc-80/Asteroids/main.py
@@ -1,13 +1,14 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
-
+from shot import Shot
 
 def main():
 	pygame.init()
@@ -17,8 +18,10 @@ def main():
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
 
 	Asteroid.containers = (asteroids, updatable, drawable)
+	Shot.cointeiners = (shots, updatable, drawable)
 	AsteroidField.containers = updatable
 	asteroid_field = AsteroidField()
 
@@ -32,11 +35,16 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		
+
 		updatable.update(dt)
 
+		for asteroid in asteroids:
+			if asteroid.collides_with(player):
+				print("Game over!!!")
+				sys.exit()
+
 		screen.fill("black")
-		
+
 		for obj in drawable:
             		obj.draw(screen)
 
